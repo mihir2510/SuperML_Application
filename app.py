9a64fbc8dbc3040be596651599673076ffa517a1
arnav <mankadarnav@gmail.com>
2021-03-21 00:46:34 +0530

    'result-generator'

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, render_template, send_from_directory
 import settings
 from auto_machine_learning.AutoML.automl import automl_run
 from pandas import read_csv
+from auto_machine_learning.AutoML.auto_model_trainer import auto_trainer
 port = 5000
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(settings)
@@ -51,6 +52,34 @@ def process():
     # return {
     #     'this': 'works'
     # }
+
+@app.route('/result-generator')
+def result_gen():
+    return render_template('result_gen.html')
+
+@app.route('/process-result-gen', methods=['POST'])
+def process_result_gen():
+    label = request.form['label']
+    task = request.form['task']
+    formdata = dict(list(request.form.lists()))
+    # return formdata
+    uploaded_file = request.files['dataset']
+    dataset_path = './data/{}'.format(uploaded_file.filename or 'dataset.csv')
+    uploaded_file.save(dataset_path)
+    dataset = read_csv(dataset_path)
+    pickle_path = './models/model.pickle'
+    excel_path = './excel_files/excel'
+    print(formdata)
+    models = formdata.get('models', None)
+    feature_engineering_methods = formdata.get('fe_method')
+    hpo_methods = formdata.get('hpo_method')
+    models = formdata.get('models')
+    sortby = formdata.get('metric')
+    print(sortby)
+    stats, model = auto_trainer(dataset,label,task, feature_engineering_methods=feature_engineering_methods, hpo_methods=hpo_methods, models=models, sortby = sortby[0])
+    print(stats.head())
+
+    return 'data-submitted'
 
 @app.route('/<path:path>')
 def send_js(path):
